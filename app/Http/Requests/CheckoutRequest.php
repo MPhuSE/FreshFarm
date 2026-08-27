3e948ad73dd6e00de62db2a928f17fefb6c3f236
MPhuSE <lephu422@hotmail.com>
2026-08-27 17:34:50 +0700

    Initial commit

---
+++ app/Http/Requests/CheckoutRequest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Contracts\Validation\ValidationRule;
+use Illuminate\Foundation\Http\FormRequest;
+
+class CheckoutRequest extends FormRequest
+{
+
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    public function rules(): array
+    {
+        return [
+
+        ];
+    }
+}
